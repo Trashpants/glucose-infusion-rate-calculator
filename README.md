1d120dafb24c85e06578f158a6906f928181d23c
Nash Bansal <29092604+Trashpants@users.noreply.github.com>
2020-09-08 17:38:12 +0100

    Added Segmented control to Calculator screen

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # glucose-infusion-rate-calculator
 Glucose Infusion Rate calculator for pediatricians
+
+This is a simple calculator written in React Native (using expo) to allow pediatricians quickly calculate glucose infusion rate.
--- src/views/screens/GIRCalculatorScreen.tsx
+++ src/views/screens/GIRCalculatorScreen.tsx
@@ -1,5 +1,13 @@
 import React, { useState } from "react";
-import { StyleSheet, View, ScrollView, Text } from "react-native";
+import {
+  StyleSheet,
+  View,
+  ScrollView,
+  Text,
+  KeyboardAvoidingView,
+} from "react-native";
+import Constants from "expo-constants";
+import SegmentedControl from "@react-native-community/segmented-control";
 import {
   HorizontalCardPicker,
   FormLabel,
@@ -7,8 +15,7 @@ import {
   NumberInput,
 } from "@components/index";
 import { useTheme } from "@react-navigation/native";
-
-import { calculateIVFluidGIR } from "utilities/Calculations";
+import { calculateIVFluidGIR } from "@utilities/Calculations";
 
 const kgWeights = [1, 2, 3, 4, 5, 6, 7, 8];
 
@@ -18,6 +25,7 @@ export default function GIRCalculatorScreen() {
   const [weight, setWeight] = useState<number>(0);
   const [dextrose, setDextrose] = useState<number>(0);
   const [ml, setMl] = useState<number>(0);
+  const [polyCal, setPolyCal] = useState<number>(0);
 
   const styles = StyleSheet.create({
     container: { flex: 1 },
@@ -43,48 +51,68 @@ export default function GIRCalculatorScreen() {
       borderColor: theme.colors.text,
       paddingBottom: 0,
     },
+    polyCalSegments: {
+      margin: 12,
+    },
   });
 
   return (
     <View style={styles.container}>
-      <ScrollView>
-        <Spacer />
-        <FormLabel>Childs Weight</FormLabel>
-        <HorizontalCardPicker
-          items={kgWeights}
-          measurement="KG"
-          onPress={(weightVal) => {
-            setWeight(weightVal);
-          }}
-        />
+      <KeyboardAvoidingView
+        style={styles.container}
+        behavior="padding"
+        keyboardVerticalOffset={Constants.statusBarHeight + 36}
+      >
+        <ScrollView keyboardShouldPersistTaps="handled">
+          <Spacer />
+          <FormLabel>Childs Weight</FormLabel>
+          <HorizontalCardPicker
+            items={kgWeights}
+            measurement="KG"
+            onPress={(weightVal) => {
+              setWeight(weightVal);
+            }}
+          />
 
-        <Spacer />
-        <FormLabel>IV fluids/TPN</FormLabel>
-        <NumberInput
-          onChange={(dex) => {
-            setDextrose(dex);
-          }}
-          length={2}
-          ending="Dextrose %"
-        />
+          <Spacer />
+          <FormLabel>IV fluids/TPN</FormLabel>
+          <NumberInput
+            onChange={(dex) => {
+              setDextrose(dex);
+            }}
+            length={2}
+            ending="Dextrose %"
+          />
 
-        <Spacer />
-        <NumberInput
-          onChange={(m) => {
-            setMl(m);
-          }}
-          length={2}
-          ending="ml/hr"
-        />
-        <Spacer />
-        <FormLabel>IV fluid GIR</FormLabel>
-        <View style={styles.ivGIRContainer}>
-          <Text style={styles.ivGIR} numberOfLines={1} adjustsFontSizeToFit>
-            {calculateIVFluidGIR(weight, dextrose, ml)}
-            <Text style={styles.ivGIRMeasurement}> mg/kg/min</Text>
-          </Text>
-        </View>
-      </ScrollView>
+          <Spacer />
+          <NumberInput
+            onChange={(m) => {
+              setMl(m);
+            }}
+            length={2}
+            ending="ml/hr"
+          />
+          <Spacer />
+          <FormLabel>IV fluid GIR</FormLabel>
+          <View style={styles.ivGIRContainer}>
+            <Text style={styles.ivGIR} numberOfLines={1} adjustsFontSizeToFit>
+              {calculateIVFluidGIR(weight, dextrose, ml)}
+              <Text style={styles.ivGIRMeasurement}> mg/kg/min</Text>
+            </Text>
+          </View>
+
+          <Spacer />
+          <FormLabel>PolyCal</FormLabel>
+          <SegmentedControl
+            style={styles.polyCalSegments}
+            values={["None", "1/4 tsp", "1/2 tsp", "3/4 tsp", "1 tsp"]}
+            selectedIndex={polyCal}
+            onChange={(event) => {
+              setPolyCal(event.nativeEvent.selectedSegmentIndex);
+            }}
+          />
+        </ScrollView>
+      </KeyboardAvoidingView>
     </View>
   );
 }
